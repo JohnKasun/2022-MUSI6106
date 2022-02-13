1e7286354cd5eac9066b7ee383df0d5c3ffa5d2b
John Kasun <johnkasun@live.com>
2022-02-13 12:01:40 -0500

    Implement all tests

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -172,11 +172,34 @@ int main(int argc, char* argv[])
 
 }
 
+void     showClInfo()
+{
+    cout << "MUSI6106 Assignment Executable" << endl;
+    cout << "(c) 2014-2022 by Alexander Lerch" << endl;
+    cout << endl;
+
+    return;
+}
+
+bool test1();
+void test2();
+bool test3();
+bool test4();
+bool test5();
+
+void runTests()
+{
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+}
+
+//Display Input and Output Audio Buffers
 void displayIOBuffers(float** ppfAudioInputBuffer, float** ppfAudioOutputBuffer, int iNumChannels, int iNumSamples)
 {
-    static int iNumBlock = 0;
-
-    std::cout << "\n===Block " << ++iNumBlock << "=============================" << std::endl;
+    std::cout << "\n=======================================" << std::endl;
     std::cout << "Input: " << std::endl;
     for (int channel = 0; channel < iNumChannels; channel++)
     {
@@ -201,30 +224,40 @@ void displayIOBuffers(float** ppfAudioInputBuffer, float** ppfAudioOutputBuffer,
     std::cout << "=======================================" << std::endl;
 }
 
+//Displays single Audio Buffer
+void displayBuffer(float** ppfAudioBuffer, int iNumChannels, int iNumSamples)
+{
+    std::cout << "Buffer: " << std::endl;
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        std::cout << "\tChannel " << channel << ": [";
+        for (int sample = 0; sample < iNumSamples; sample++)
+        {
+            std::cout << ppfAudioBuffer[channel][sample] << " ";
+        }
+        std::cout << "]" << std::endl;
+    }
+    std::cout << "=======================================" << std::endl;
+}
+
+//Converts string to lowercase
 void toLower(std::string &sStringToLower)
 {
     for (char& letter : sStringToLower)
         letter = tolower(letter);
 }
 
-void     showClInfo()
-{
-    cout << "MUSI6106 Assignment Executable" << endl;
-    cout << "(c) 2014-2022 by Alexander Lerch" << endl;
-    cout  << endl;
-
-    return;
-}
-
+//Test for output being zero when input freq matches feedforward
+//If fPeriod * fSampleRate is a whole number, then all values will be practically zero for all samples once the delay comes in.
+//For this set of values, all values will be zero starting at sample 4.
+//This works best for high sample rates and high frequencies
 bool test1()
 {
-    //If fPeriod * fSampleRate is a whole number, then all values will be practically zero for all samples once the delay comes in.
-    //For this set of values, all values will be zero starting at sample 441.
     const float fTwoPi = 2.0f * M_PI;
     const int iNumChannels = 2;
-    const int iNumSamples = 1000000;
-    const float fSampleRate = 44100;
-    const int iInputFreq = 100;
+    const int iNumSamples = 1000;
+    const float fSampleRate = 40000;
+    const float iInputFreq = 10000;
     const float fPeriod = 1.0f / iInputFreq;
     const int iSampleDelayComesIn = fPeriod * fSampleRate;
 
@@ -242,16 +275,19 @@ bool test1()
         float fCurrentAngle = 0.0f;
         for (int sample = 0; sample < iNumSamples; sample++)
         {
-            ppfInputBuffer[channel][sample] = sinf(fCurrentAngle*sample);
+            ppfInputBuffer[channel][sample] = sin(fCurrentAngle * sample);
+            fCurrentAngle += fAngleDelta;
+            while (fCurrentAngle >= fTwoPi)
+                fCurrentAngle -= fTwoPi;
         }
-        while (fCurrentAngle >= fTwoPi)
-            fCurrentAngle -= fTwoPi;
+
     }
-            
 
     CCombFilterIf* phCombFilter = 0;
     CCombFilterIf::create(phCombFilter);
     phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombFIR, 1, fSampleRate, iNumChannels);
+
+    //Set delay value to the period length and set gain to be inverting
     phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, fPeriod);
     phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, -1);
     phCombFilter->process(ppfInputBuffer, ppfOutputBuffer, iNumSamples);
@@ -260,13 +296,13 @@ bool test1()
     for (int channel = 0; channel < iNumChannels; channel++)
         for (int sample = iSampleDelayComesIn; sample < iNumSamples; sample++)
         {
-            if (ppfOutputBuffer[channel][sample] > 1E-6)
-            {
-                passed = false;
-            }
-                
+             //Test fails if processed value is greater than 1 * 10^-4
+             if (ppfOutputBuffer[channel][sample] > 1E-4)
+                 passed = false;
         }
 
+    //display Output buffer here if you'd like
+    //displayBuffer(ppfOutputBuffer, iNumChannels, iNumSamples);
     CCombFilterIf::destroy(phCombFilter);
     for (int channel = 0; channel < iNumChannels; channel++)
     {
@@ -275,12 +311,312 @@ bool test1()
     }
     delete[] ppfInputBuffer;
     delete[] ppfOutputBuffer;
-        
+
     return passed;
 }
 
-void runTests()
+//Test for amount of magnitude increase/decrease if input freq matches feedback
+//The output sample is divided by the input sample and stored in the ppfMagnitudeRatios buffer
+void test2()
 {
-    test1();
+    const float fTwoPi = 2.0f * M_PI;
+    const int iNumChannels = 2;
+    const int iNumSamples = 10000;
+    const float fSampleRate = 40000;
+    const int iInputFreq = 1000;
+    const float fPeriod = 1.0f / iInputFreq;
+    const int iSampleDelayComesIn = fPeriod * fSampleRate;
+
+    float** ppfInputBuffer = new float* [iNumChannels];
+    float** ppfOutputBuffer = new float* [iNumChannels];
+    float** ppfMagnitudeRatios = new float* [iNumChannels];
+    for (int i = 0; i < iNumChannels; i++)
+    {
+        ppfInputBuffer[i] = new float[iNumSamples] {};
+        ppfOutputBuffer[i] = new float[iNumSamples] {};
+        ppfMagnitudeRatios[i] = new float[iNumSamples] {};
+    }
+
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        float fAngleDelta = fTwoPi * iInputFreq / fSampleRate;
+        float fCurrentAngle = 0.0f;
+        for (int sample = 0; sample < iNumSamples; sample++)
+        {
+            ppfInputBuffer[channel][sample] = sin(fCurrentAngle * sample);
+            fCurrentAngle += fAngleDelta;
+            while (fCurrentAngle >= fTwoPi)
+                fCurrentAngle -= fTwoPi;
+        }
+    }
+
+
+    CCombFilterIf* phCombFilter = 0;
+    CCombFilterIf::create(phCombFilter);
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombIIR, 1, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, fPeriod);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+    phCombFilter->process(ppfInputBuffer, ppfOutputBuffer, iNumSamples);
+
+    bool passed = true;
+    for (int channel = 0; channel < iNumChannels; channel++)
+        for (int sample = 0; sample < iNumSamples; sample++)
+        {
+            float fInputSample = ppfInputBuffer[channel][sample];
+            float fOutputSample = ppfOutputBuffer[channel][sample];
+            float fMagnitudeRatio{INFINITY};
+            if (fInputSample != 0)
+                fMagnitudeRatio = fOutputSample / fInputSample;
+            ppfMagnitudeRatios[channel][sample] = fMagnitudeRatio;
+        }
+
+    //display buffer here if you'd like
+    //displayBuffer(ppfMagnitudeRatios, iNumChannels, iNumSamples);
+    CCombFilterIf::destroy(phCombFilter);
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        delete[] ppfInputBuffer[channel];
+        delete[] ppfOutputBuffer[channel];
+        delete[] ppfMagnitudeRatios[channel];
+    }
+    delete[] ppfInputBuffer;
+    delete[] ppfOutputBuffer;
+    delete[] ppfMagnitudeRatios;
+
+}
+
+//Helper function for processing a block of audio data with comb filter
+//iStartSample is the offset corresponding to the current read/write position of the original buffer
+void processBlock(float** ppfInputBuffer, float** ppfOutputBuffer, CCombFilterIf* phCombFilter, int iNumChannels, int iStartSample, int iBlockSize)
+{
+    float** ppfTempInBuffer = new float* [iNumChannels];
+    float** ppfTempOutBuffer = new float* [iNumChannels];
+    for (int i = 0; i < iNumChannels; i++)
+    {
+        ppfTempInBuffer[i] = new float[iBlockSize] {0};
+        ppfTempOutBuffer[i] = new float[iBlockSize] {0};
+    }
+
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        for (int sample = 0; sample < iBlockSize; sample++)
+        {
+            ppfTempInBuffer[channel][sample] = ppfInputBuffer[channel][sample + iStartSample];
+        }
+        phCombFilter->process(ppfTempInBuffer, ppfTempOutBuffer, iBlockSize);
+        for (int sample = 0; sample < iBlockSize; sample++)
+        {
+            ppfOutputBuffer[channel][sample + iStartSample] = ppfTempOutBuffer[channel][sample];
+        }
+    }
+
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        delete[] ppfTempInBuffer[channel];
+        delete[] ppfTempOutBuffer[channel];
+    }
+    delete[] ppfTempInBuffer;
+    delete[] ppfTempOutBuffer;
+}
+
+//Test for varying input block size
+//Uses processBlock(...) helper function with the following block sizes: 1000, 4000, 5000
+bool test3()
+{
+    const int iNumChannels = 1;
+    const int iNumSamples = 10000;
+    const float fSampleRate = 44100;
+
+    float** ppfInputBuffer = new float* [iNumChannels];
+    float** ppfOutputBufferFIR = new float* [iNumChannels];
+    float** ppfOutputBufferIIR = new float* [iNumChannels];
+    float** ppfOutputBufferFIRBlock = new float* [iNumChannels];
+    float** ppfOutputBufferIIRBlock = new float* [iNumChannels];
+    for (int i = 0; i < iNumChannels; i++)
+    {
+        ppfInputBuffer[i] = new float[iNumSamples] {0};
+        ppfOutputBufferFIR[i] = new float[iNumSamples] {0};
+        ppfOutputBufferIIR[i] = new float[iNumSamples] {0};
+        ppfOutputBufferFIRBlock[i] = new float[iNumSamples] {0};
+        ppfOutputBufferIIRBlock[i] = new float[iNumSamples] {0};
+        for (int sample = 0; sample < iNumSamples; sample++)
+            ppfInputBuffer[i][sample] = static_cast<float>(sample);
+    }
+
+
+    CCombFilterIf* phCombFilter = 0;
+    CCombFilterIf::create(phCombFilter);
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombFIR, 1, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 0.01);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+
+    processBlock(ppfInputBuffer, ppfOutputBufferFIRBlock, phCombFilter, iNumChannels, 0, 1000);
+    processBlock(ppfInputBuffer, ppfOutputBufferFIRBlock, phCombFilter, iNumChannels, 1000, 4000);
+    processBlock(ppfInputBuffer, ppfOutputBufferFIRBlock, phCombFilter, iNumChannels, 5000, 5000);
+
+    phCombFilter->reset();
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombIIR, 1, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 0.01);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+    
+    processBlock(ppfInputBuffer, ppfOutputBufferIIRBlock, phCombFilter, iNumChannels, 0, 1000);
+    processBlock(ppfInputBuffer, ppfOutputBufferIIRBlock, phCombFilter, iNumChannels, 1000, 4000);
+    processBlock(ppfInputBuffer, ppfOutputBufferIIRBlock, phCombFilter, iNumChannels, 5000, 5000);
+
+    phCombFilter->reset();
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombFIR, 1, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 0.01);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+    phCombFilter->process(ppfInputBuffer, ppfOutputBufferFIR, iNumSamples);
+
+    phCombFilter->reset();
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombIIR, 1, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 0.01);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+    phCombFilter->process(ppfInputBuffer, ppfOutputBufferIIR, iNumSamples);
+
+    bool passed = true;
+    for (int channel = 0; channel < iNumChannels; channel++)
+        for (int sample = 0; sample < iNumSamples; sample++)
+        {
+            //Calculates diff between block-by-block processing and all-at-once processing
+            float firDiff = ppfOutputBufferFIR[channel][sample] - ppfOutputBufferFIRBlock[channel][sample];
+            float iirDiff = ppfOutputBufferIIR[channel][sample] - ppfOutputBufferIIRBlock[channel][sample];
+            if (firDiff != 0 || iirDiff != 0)
+                passed = false;
+        }
+
+    //displayBuffer(ppfOutputBufferFIRBlock, iNumChannels, iNumSamples);
+    //displayBuffer(ppfOutputBufferIIRBlock, iNumChannels, iNumSamples);
+    CCombFilterIf::destroy(phCombFilter);
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        delete[] ppfInputBuffer[channel];
+        delete[] ppfOutputBufferFIR[channel];
+        delete[] ppfOutputBufferIIR[channel];
+        delete[] ppfOutputBufferFIRBlock[channel];
+        delete[] ppfOutputBufferIIRBlock[channel];
+    }
+    delete[] ppfInputBuffer;
+    delete[] ppfOutputBufferFIR;
+    delete[] ppfOutputBufferIIR;
+    delete[] ppfOutputBufferFIRBlock;
+    delete[] ppfOutputBufferIIRBlock;
+
+    return passed;
+
+}
+
+//Test for zero input signal
+//Output should be all zeros
+bool test4()
+{
+    const int iNumChannels = 1;
+    const int iNumSamples = 10000;
+    const float fSampleRate = 44100;
+
+    float** ppfInputBuffer = new float* [iNumChannels];
+    float** ppfOutputBufferFIR = new float* [iNumChannels];
+    float** ppfOutputBufferIIR = new float* [iNumChannels];
+    for (int i = 0; i < iNumChannels; i++)
+    {
+        ppfInputBuffer[i] = new float[iNumSamples] {0};
+        ppfOutputBufferFIR[i] = new float[iNumSamples] {0};
+        ppfOutputBufferIIR[i] = new float[iNumSamples] {0};
+    }
+
+    CCombFilterIf* phCombFilter = 0;
+    CCombFilterIf::create(phCombFilter);
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombFIR, 1, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 0.01);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+    phCombFilter->process(ppfInputBuffer, ppfOutputBufferFIR, iNumSamples);
+
+    phCombFilter->reset();
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombIIR, 1, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 0.01);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+    phCombFilter->process(ppfInputBuffer, ppfOutputBufferIIR, iNumSamples);
+
+    bool passed = true;
+    for (int channel = 0; channel < iNumChannels; channel++)
+        for (int sample = 0; sample < iNumSamples; sample++)
+        {
+            //Ensures output values are zero
+            float firValue = ppfOutputBufferFIR[channel][sample];
+            float iirValue = ppfOutputBufferIIR[channel][sample];
+            if (firValue != 0 || iirValue != 0)
+                passed = false;
+        }
+
+    //displayBuffer(ppfOutputBufferFIR, iNumChannels, iNumSamples);
+    //displayBuffer(ppfOutputBufferIIR, iNumChannels, iNumSamples);
+    CCombFilterIf::destroy(phCombFilter);
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        delete[] ppfInputBuffer[channel];
+        delete[] ppfOutputBufferFIR[channel];
+        delete[] ppfOutputBufferIIR[channel];
+    }
+    delete[] ppfInputBuffer;
+    delete[] ppfOutputBufferFIR;
+    delete[] ppfOutputBufferIIR;
+
+    return passed;
+}
+
+//Tests changing IIR parameter settings between process blocks
+//Here, the delay parameter is changed from 6 samples to 8 samples. This will place the following delay value on sample 14 instead of sample 12.
+//The gain value is also changed but it will be updated for the delay on sample 6 and all following delays.
+bool test5()
+{
+    const int iNumChannels = 1;
+    const int iNumSamples = 15;
+    const float fSampleRate = 1;
+
+    float** ppfInputBuffer = new float* [iNumChannels];
+    float** ppfOutputBufferIIR = new float* [iNumChannels];
+    for (int i = 0; i < iNumChannels; i++)
+    {
+        ppfInputBuffer[i] = new float[iNumSamples] {1};
+        ppfOutputBufferIIR[i] = new float[iNumSamples] {0};
+    }
+
+
+    CCombFilterIf* phCombFilter = 0;
+    CCombFilterIf::create(phCombFilter);
+    phCombFilter->init(CCombFilterIf::CombFilterType_t::kCombIIR, 15, fSampleRate, iNumChannels);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 6);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.5);
+
+    processBlock(ppfInputBuffer, ppfOutputBufferIIR, phCombFilter, iNumChannels, 0, 5);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamDelay, 8);
+    phCombFilter->setParam(CCombFilterIf::FilterParam_t::kParamGain, 0.25);
+    processBlock(ppfInputBuffer, ppfOutputBufferIIR, phCombFilter, iNumChannels, 5, 5);
+    processBlock(ppfInputBuffer, ppfOutputBufferIIR, phCombFilter, iNumChannels, 10, 5);
+
+    bool passed = true;
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        //Tests for expected values
+        if (ppfOutputBufferIIR[channel][0] != 1)
+            passed = false;
+        else if (ppfOutputBufferIIR[channel][6] != 0.25)
+            passed = false;
+        else if (ppfOutputBufferIIR[channel][14] != 0.0625)
+            passed = false;
+    }
+
+    //displayIOBuffers(ppfInputBuffer, ppfOutputBufferIIR, iNumChannels, iNumSamples);
+    CCombFilterIf::destroy(phCombFilter);
+    for (int channel = 0; channel < iNumChannels; channel++)
+    {
+        delete[] ppfInputBuffer[channel];
+        delete[] ppfOutputBufferIIR[channel];
+    }
+    delete[] ppfInputBuffer;
+    delete[] ppfOutputBufferIIR;
+
+    return passed;
 }
 
