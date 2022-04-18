74bd65ea15f12d67cfd9d9ecfb62152bd5313ed7
John Kasun <johnkasun@live.com>
2022-04-18 18:53:35 -0400

    Add test and ground buffers for testing

--- src/Tests/Tests/Test_FastConv.cpp
+++ src/Tests/Tests/Test_FastConv.cpp
@@ -23,13 +23,55 @@ namespace fastconv_test {
     protected:
         void SetUp() override
         {
+            srand(time(0));
+            m_pCFastConv = new CFastConv();
         }
 
         virtual void TearDown()
         {
+            m_pCFastConv->reset();
+            delete m_pCFastConv;
+            m_pCFastConv = 0;
+
+            if (m_pfInput)
+            {
+                delete[] m_pfInput;
+                m_pfInput = 0;
+            }
+            if (m_pfIr)
+            {
+                delete[] m_pfIr;
+                m_pfIr = 0;
+            }
+            if (m_pfTestOutput)
+            {
+                delete[] m_pfTestOutput;
+                m_pfTestOutput = 0;
+            }
+            if (m_pfTestTail)
+            {
+                delete[] m_pfTestTail;
+                m_pfTestTail = 0;
+            }
+            if (m_pfGroundOutput)
+            {
+                delete[] m_pfGroundOutput;
+                m_pfGroundOutput = 0;
+            }
+            if (m_pfGroundTail)
+            {
+                delete[] m_pfGroundTail;
+                m_pfGroundTail = 0;
+            }
         }
 
-        CFastConv *m_pCFastConv = 0;
+        CFastConv* m_pCFastConv = 0;
+        float* m_pfInput = 0;
+        float* m_pfIr = 0;
+        float* m_pfTestOutput = 0;
+        float* m_pfTestTail = 0;
+        float* m_pfGroundOutput = 0;
+        float* m_pfGroundTail = 0;
     };
 
     TEST_F(FastConv, EmptyTest)
